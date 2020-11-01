1ddcb8935ead919c74af8e60690c2b1a454e8866
Vedran Ljubović <vljubovic@smartnet.ba>
2020-11-01 12:58:15 +0100

    Initial import

---
+++ src/ba/unsa/etf/rpr/Main.java
@@ -0,0 +1,11 @@
+package ba.unsa.etf.rpr;
+
+import java.util.*;
+import java.util.stream.Stream;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Ugodno rješavanje zadaće 1 :)");
+    }
+}
